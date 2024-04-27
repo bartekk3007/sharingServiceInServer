c09599ca977b8f225177edfe31e18298d04203fc
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-04-27 23:13:48 +0200

    Add files via upload

---
+++ EndpointKlient3/EndpointKlient3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace EndpointKlient3
+{
+    public class HandlerZad6 : EndpointKlient3.ServiceReference1.IZadanie6Callback
+    {
+        public void Wynik(int wyn)
+        {
+            Console.WriteLine($"Wynik zwrotny: {wyn}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client5 = new EndpointKlient3.ServiceReference1.Zadanie5Client();
+            Console.WriteLine(client5.ScalNapisy(client5.ScalNapisy("zadanie", " dziala"), " dobrze"));
+            Console.ReadKey();
+            ((IDisposable)client5).Dispose();
+
+            var client6 = new EndpointKlient3.ServiceReference1.Zadanie6Client(new InstanceContext(new HandlerZad6()));
+            client6.Dodaj(1, 1);
+            Console.ReadKey();
+            ((IDisposable)client6).Dispose();
+        }
+    }
+}
